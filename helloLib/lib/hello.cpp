3b9683cb2fa21d692d2d6deda4a02dbc704d0286
Ran <soleyran93@gmail.com>
2022-09-20 01:04:10 +0800

    FIRST

---
+++ helloLib/lib/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "hello.h"
+
+void PrintHello()
+{
+	std::cout << " hello " << std::endl;
+}
---
+++ main/build/bin/CMakeFiles/main.dir/main.o.d
@@ -0,0 +1,3 @@
+bin/CMakeFiles/main.dir/main.o: \
+  /Users/ranshu/Desktop/cmakeTest/main/src/main.cpp \
+  /Users/ranshu/Desktop/cmakeTest/main/include/hello.h
